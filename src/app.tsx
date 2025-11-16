5f344941ef4ea1b4b1e7201115672300be67409c
Theofilus Handoyo <realth99@gmail.com>
2025-11-16 15:15:56 +0700

    feat: add tasks page

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,27 @@
+import { Tasks } from "./modules/task/tasks";
+
 function App() {
   return (
     <div className="flex justify-center">
       <div>
-        <h1>Task management</h1>
+        {/* <h1>Task management</h1>
         <ul className="flex flex-col">
           <Task title="breakfast" />
           <Task title="Lunch" />
           <Task title="dinner" />
         </ul>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click me</Button>
+        </div> */}
+      </div>
+      <div className="flex justify-center">
+        <main className="w-full max-w-lg space-y-8">
+          <h1 className="my-4 text-2xl font-bold text-sky-700">
+            Task Management
+          </h1>
+
+          <Tasks />
+        </main>
       </div>
     </div>
   );
---
+++ src/modules/task/tasks.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button";
+import { EyeIcon, TrashIcon } from "lucide-react";
+import { useState } from "react";
+
+type Task = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+type Tasks = Task[];
+
+const initialDataTasks: Tasks = [
+  { id: 1, title: "Breakfast", isDone: true },
+  { id: 2, title: "Lunch", isDone: false },
+  { id: 3, title: "Dinner", isDone: false },
+];
+
+export function Tasks() {
+  const [tasks, setTasks] = useState(initialDataTasks);
+
+  function handleDelete(id: number) {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <ul className="flex flex-col gap-4">
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <TaskItem task={task} handleDelete={() => handleDelete(task.id)} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export function TaskItem({
+  task,
+  handleDelete,
+}: {
+  task: Task;
+  handleDelete: () => void;
+}) {
+  return (
+    <section className="flex justify-between gap-4 rounded-lg bg-sky-100 p-4">
+      <div>
+        <h2 className="text-lg font-bold">{task.title}</h2>
+        <p>{task.isDone ? "✅ Done" : "📝 Todo"}</p>
+      </div>
+      <div className="flex gap-2">
+        <Button size="xs">
+          <EyeIcon className="size-3" />
+          <span className="text-xs">View</span>
+        </Button>
+        <Button variant="destructive" size="xs" onClick={handleDelete}>
+          <TrashIcon className="size-3" />
+          <span className="text-xs">Delete</span>
+        </Button>
+      </div>
+    </section>
+  );
+}
