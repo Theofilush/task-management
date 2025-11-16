836c2c85cda7cad16bd0252eb53698ce4373308b
Theofilus Handoyo <realth99@gmail.com>
2025-11-16 15:25:17 +0700

    feat: add counter increment dan focus input

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import { Tasks } from "./modules/task/tasks";
 function App() {
   return (
     <div className="flex justify-center">
-      <div>
-        {/* <h1>Task management</h1>
+      {/* <div>
+        <h1>Task management</h1>
         <ul className="flex flex-col">
           <Task title="breakfast" />
           <Task title="Lunch" />
@@ -12,17 +12,15 @@ function App() {
         </ul>
         <div className="flex min-h-svh flex-col items-center justify-center">
           <Button>Click me</Button>
-        </div> */}
-      </div>
-      <div className="flex justify-center">
-        <main className="w-full max-w-lg space-y-8">
-          <h1 className="my-4 text-2xl font-bold text-sky-700">
-            Task Management
-          </h1>
+        </div> 
+      </div> */}
+      <main className="w-full max-w-lg space-y-8">
+        <h1 className="my-4 text-2xl font-bold text-sky-700">
+          Task Management
+        </h1>
 
-          <Tasks />
-        </main>
-      </div>
+        <Tasks />
+      </main>
     </div>
   );
 }
